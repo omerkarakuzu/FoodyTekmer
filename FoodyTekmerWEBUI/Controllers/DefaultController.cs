344d13c58336ad4c88a6559c243267f7c88f2916
omerkarakuzu <80119008+omerkarakuzu@users.noreply.github.com>
2024-02-25 21:01:22 +0300

    Add project files.

---
+++ FoodyTekmerWEBUI/Controllers/DefaultController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodyTekmerWEBUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.title1 = "Ürünler";
+            ViewBag.title2 = "Anasayfa";
+            ViewBag.title3 = "Sayfalar";
+            ViewBag.title4 = "Ürün Listesi";
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialPageHeader()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFirmVisit()
+        {
+            return PartialView();
+        }
+
+    }
+}
